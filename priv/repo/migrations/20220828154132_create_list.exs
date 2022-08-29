4c61439ec5daf34b2cc3e6a6d34e4ed60e2f2e0c
Dickson <dicksonmelo@gmail.com>
2022-08-29 18:49:40 -0300

    chore: add list schema

--- priv/repo/migrations/20220828154132_create_list.exs
+++ priv/repo/migrations/20220828154132_create_list.exs
@@ -2,9 +2,13 @@ defmodule BackendAppFeirinha.Repo.Migrations.CreateList do
   use Ecto.Migration
 
   def change do
-    create table(:list) do
+    create table(:list, primary_key: false) do
+      add(:id, :uuid, primary_key: true)
+      add(:name, :string)
 
       timestamps()
     end
+
+    create(unique_index(:list, [:name]))
   end
 end
--- priv/repo/migrations/20220828154253_create_list_item.exs
+++ priv/repo/migrations/20220828154253_create_list_item.exs
@@ -3,6 +3,10 @@ defmodule BackendAppFeirinha.Repo.Migrations.CreateListItem do
 
   def change do
     create table(:list_item) do
+      add(:item_qtd, :integer)
+      add(:brand, :string)
+      add(:product_id, references(:products))
+      add(:list_id, references(:list))
 
       timestamps()
     end
