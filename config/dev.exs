b984c594fd39c2d79ce087c72be24241a8d497f1
Dickson <dicksonmelo@gmail.com>
2022-09-01 19:34:36 -0300

    fix: changing config to production

--- config/dev.exs
+++ config/dev.exs
@@ -8,7 +8,7 @@ config :backend_app_feirinha, BackendAppFeirinha.Repo,
   database: "backend_app_feirinha_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
-  pool_size: 10
+  pool_size: 2
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
