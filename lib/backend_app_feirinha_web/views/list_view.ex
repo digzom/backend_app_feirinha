c5c99e8fe468fa3e08f411d9ad7410ce3b2e8f3b
Dickson <dicksonmelo@gmail.com>
2022-09-13 05:54:05 -0300

    chore: changing the way that the list views return data

--- lib/backend_app_feirinha_web/views/list_view.ex
+++ lib/backend_app_feirinha_web/views/list_view.ex
@@ -3,11 +3,11 @@ defmodule BackendAppFeirinhaWeb.ListView do
   alias BackendAppFeirinhaWeb.ListView
 
   def render("index.json", %{lists: lists}) do
-    %{data: render_many(lists, ListView, "list.json")}
+    render_many(lists, ListView, "list.json")
   end
 
   def render("show.json", %{list: list}) do
-    %{data: render_one(list, ListView, "list.json")}
+    render_one(list, ListView, "list.json")
   end
 
   def render("list.json", %{list: list}) do
