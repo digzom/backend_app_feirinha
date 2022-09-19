e55269911764c2086f853ef018ef7f94cd58041b
digzom <dicksonmelo@gmail.com>
2022-09-19 18:15:39 -0300

    adding tests to products (#12)

--- lib/backend_app_feirinha_web/views/error_view.ex
+++ lib/backend_app_feirinha_web/views/error_view.ex
@@ -1,5 +1,6 @@
 defmodule BackendAppFeirinhaWeb.ErrorView do
   use BackendAppFeirinhaWeb, :view
+  import Ecto.Changeset, only: [traverse_errors: 2]
 
   # If you want to customize a particular status code
   # for a certain format, you may uncomment below.
@@ -12,5 +13,19 @@ defmodule BackendAppFeirinhaWeb.ErrorView do
   # "Not Found".
   def template_not_found(template, _assigns) do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
+  end
+
+  def render("400.json", %{result: result}) do
+    %{message: translate_errors(result)}
+  end
+
+  defp translate_errors({:error, changeset}) do
+    # seria bom tentar entender isso aqui
+
+    traverse_errors(changeset, fn {msg, opts} ->
+      Enum.reduce(opts, msg, fn {key, value}, acc ->
+        String.replace(acc, "%{#{key}}", to_string(value))
+      end)
+    end)
   end
 end
